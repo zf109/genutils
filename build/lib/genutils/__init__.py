ef82e0c2213dfdcf2017ec08d04ed27321caba40
Zhe Feng <zfeng@deloitte.co.uk>
2018-11-06 13:12:45 +0000

    clear up the repo

--- build/lib/genutils/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import fileoperation
-from . import listoperation
-from . import noneoperation
-from . import typeoperation
--- build/lib/genutils/fileoperation.py
+++
@@ -1,8 +0,0 @@
-"""
-    collection of file operations
-"""
-
-def create_file(filename):
-    """Equivalent of touch """
-    f = open(filename, 'w')
-    f.close()
